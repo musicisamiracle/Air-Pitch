0f01b597bdc1492b3f5d8785b558b0c584dcc17b
DaneThomas <musicisamiracle@yahoo.com>
2017-03-12 20:22:49 -0500

    Removed some notes.

--- Air Pitch/PitchViewController.swift
+++ Air Pitch/PitchViewController.swift
@@ -12,8 +12,7 @@ import ChameleonFramework
 import Pulsator
 import TwicketSegmentedControl
 
-/*TODO: app icons
-        remove whitespace*/
+/*TODO: app icons*/
 
 class PitchViewController: UIViewController, AVAudioPlayerDelegate, TwicketSegmentedControlDelegate {
     
